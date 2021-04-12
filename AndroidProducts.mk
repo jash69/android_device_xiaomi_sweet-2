b61b33022162751573e37bd6863b71fbca3e5c33
Yilliee <m.taha6f@gmail.com>
2021-04-12 08:32:39 +0000

    AndroidProducts : Add remaining lunch options

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,4 +17,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_sweet.mk
 
-COMMON_LUNCH_CHOICES := omni_sweet-eng
+COMMON_LUNCH_CHOICES := \
+		omni_sweet-eng \
+		omni_sweet-user \
+		omni_sweet-userdebug
